23c23d55f3e26acf5e146af3bc64be889581c621
MatteoCasu <ra-9817@student.iram.be>
2024-11-26 02:50:40 +0100

    Fichier TestProjet: Ajout du test de calcul total du projet d'achat + Mise en ordre de passage les méthodes tests

--- src/test/java/TestProjet.java
+++ src/test/java/TestProjet.java
@@ -1,14 +1,20 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 import ue1396.Projet;
 
-@DisplayName("Test de la classe Projet")
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@DisplayName("Test de la classe Projet : ")
 public class TestProjet {
     private static Projet projet;
     private static Projet mockedProject;
@@ -20,11 +26,14 @@ public class TestProjet {
     }
 
     @Nested
-    @DisplayName("Test de la classe Calcul de la TVA sur les frais de transformation")
+    @Order(1)
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    @DisplayName("Calcul de la TVA sur les frais de transformation")
     class TestCalculTVA {
         @ParameterizedTest
+        @Order(1)
         @CsvSource({"90_000.00, 5_400.00", "0.00, 0.00", "100_000.00, 6_000.00", "59_595.00, 3_575.70", "1.00, 0.06"})
-        @DisplayName("Calcul de la tva sur les frais de transformation : Validation des frais positifs")
+        @DisplayName("Validation des frais positifs")
         public void testCalculTVA( double fraisTransformation, double resultat) {
             projet.setFraisTransformation(fraisTransformation);
             Assertions.assertEquals(resultat, projet.calculTVAFraisTransformation());
@@ -32,16 +41,18 @@ public class TestProjet {
 
 
         @ParameterizedTest
+        @Order(2)
         @CsvSource({"92_123.89, 5_527.4334"})
-        @DisplayName("Calcul de la tva sur les frais de transformation : Validation des frais arrondis")
+        @DisplayName("Validation des frais arrondis")
         public void testCalculTVA2( double fraisTransformation, double resultat) {
             projet.setFraisTransformation(fraisTransformation);
             Assertions.assertEquals(resultat, projet.calculTVAFraisTransformation());
         }
 
         @ParameterizedTest
+        @Order(3)
         @ValueSource(doubles = { -90_000.00, -25_000.00})
-        @DisplayName("Calcul de la tva sur les frais de transformation : Validation des frais negatifs")
+        @DisplayName("Validation des frais negatifs")
         public void testCalculTVA3(double fraisTransformation) {
             projet.setFraisTransformation(fraisTransformation);
             Assertions.assertThrows(Exception.class, () -> projet.calculTVAFraisTransformation());
@@ -49,10 +60,11 @@ public class TestProjet {
     }
 
     @Nested
-    @DisplayName("Test de la classe Calcul du droit de l'abbatement")
+    @Order(2)
+    @DisplayName("Calcul du droit de l'abbatement")
     class TestCalculDroitAbbatement {
         @ParameterizedTest
-        @CsvSource({"200_000, 40_000", "400_000, 33_333.33", "500_000, 20_000", "600_000, 20_000"})
+        @CsvSource({"100_000, 40_000", "200_000, 40_000", "400_000, 33_333.33", "500_000, 20_000", "600_000, 20_000"})
         @DisplayName("Calcul de l'abbatement")
         public void testCalculAbattement(double prixHabitation, double abattement) {
             projet.setPrixHabitation(prixHabitation);
@@ -61,30 +73,46 @@ public class TestProjet {
     }
 
     @Nested
-    @DisplayName("Test de la classe Calcul du droit d'enregistrement")
+    @Order(3)
+    @DisplayName("Calcul du droit d'enregistrement")
     class TestCalculDroitEnregistrement {
         @ParameterizedTest
         @CsvSource({"350_000.00, 40_000.00, 740, 18_599.999584257603", "180_000.00, 40_000.00, 575, 8_400.00", "100_000.00, 40_000.00, 700, 3_600.00"})
-        @DisplayName("Calcul du droit d'enregistrement : Revenu Cadastral inferieur à 745")
+        @DisplayName("Revenu Cadastral inferieur à 745")
         public void calculDroitEnregistrementRevenuCadastralInferieur745( double prixHabitation, double abattement, int revenuCadastral, double resultat) {
-            Assertions.assertAll(() -> {
                 mockedProject.setPrixHabitation(prixHabitation);
                 Mockito.doReturn(abattement).when(mockedProject).calculAbattement();
                 mockedProject.setRevenuCadastral(revenuCadastral);
                 Assertions.assertEquals(resultat, mockedProject.calculDroitEnregistrement(), 0.001);
-            });
+            
         }
 
         @ParameterizedTest
         @CsvSource({"400_000.00, 33_333.333, 746, 45_833.333"})
-        @DisplayName("Calcul du droit d'enregistrement : Revenu Cadastral superieur à 745")
+        @DisplayName("Revenu Cadastral superieur à 745")
         public void calculDroitEnregistrementRevenuCadastralSuperieur745( double prixHabitation, double abattement, int revenuCadastral, double resultat) {
-            Assertions.assertAll(() -> {
                 mockedProject.setPrixHabitation(prixHabitation);
                 Mockito.doReturn(abattement).when(mockedProject).calculAbattement();
                 mockedProject.setRevenuCadastral(revenuCadastral);
                 Assertions.assertEquals(resultat, mockedProject.calculDroitEnregistrement(), 0.001);
-            });
+            
+        }
+    }
+
+    @Nested
+    @DisplayName("Calcul total du projet d'achat ")
+    class TestCalculTotalProjetAchat {
+        @ParameterizedTest
+        @CsvSource({"100_000.00, 4_500.00, 3_600.00, 50_000.00, 3_600.00, 161_700.00"})
+        @DisplayName("(Mockito)")
+        public void testCalculTotalProjetAchat(double prixHabitation, double fraisNotaireAchat, double calculDroitEnregistrement, double fraisTransformation, double tvaFraisTransformation, double resultat) {
+            mockedProject.setPrixHabitation(prixHabitation);
+            mockedProject.setFraisTransformation(fraisTransformation);
+            mockedProject.setFraisNotaireAchat(fraisNotaireAchat);
+            mockedProject.setRevenuCadastral(700);
+            Mockito.doReturn(tvaFraisTransformation).when(mockedProject).calculTVAFraisTransformation();
+            Mockito.doReturn(calculDroitEnregistrement).when(mockedProject).calculDroitEnregistrement();
+            Assertions.assertEquals(resultat, mockedProject.calculTotalProjetAchat());
         }
     }
 }
